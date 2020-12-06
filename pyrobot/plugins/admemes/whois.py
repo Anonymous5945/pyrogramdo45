b0f7881a9a25bdd6f27b2ba88de73b2f75722328
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-12-06 09:30:58 +0530

    Update whois.py

--- pyrobot/plugins/admemes/whois.py
+++ pyrobot/plugins/admemes/whois.py
@@ -33,35 +33,67 @@ async def who_is(client, message):
     if from_user is None:
         await status_message.edit("no valid user_id / message specified")
     else:
-        message_out_str = ""
-        message_out_str += f"ID: <code>{from_user.id}</code>\n"
-        message_out_str += "First Name: "
-        message_out_str += f"<a href='tg://user?id={from_user.id}'>"
-        message_out_str += from_user.first_name or ""
-        message_out_str += "</a>\n"
-        last_name = from_user.last_name or ""
-        message_out_str += f"Last Name: {last_name}\n"
-        dc_id = from_user.dc_id or "[🙏 no profile photo 👀]"
-        message_out_str += f"DC ID: <code>{dc_id}</code>\n"
-        if message.chat.type in (("supergroup", "channel")):
-            try:
-                chat_member_p = await message.chat.get_member(from_user.id)
-                joined_date = datetime.fromtimestamp(
-                    chat_member_p.joined_date or time.time()
-                ).strftime("%Y.%m.%d %H:%M:%S")
-                message_out_str += (
-                    "<b>Joined On</b>: <code>"
-                    f"{joined_date}"
-                    "</code>\n"
-                )
-            except UserNotParticipant:
-                pass
-        chat_photo = from_user.photo
-        if chat_photo:
-            local_user_photo = await client.download_media(
-                message=chat_photo.big_file_id
+        m
+        phone = from_user.phone_number or "Number is hidden"
+        last_scene = from_user.last_online_date or "Not available"
+        last_name = from_user.last_name or "Not set by User"
+        first_name = from_user.first_name or "Not set by User"
+        scam = from_user.is_scam or "Not a scam"
+        is_bot = from_user.is_bot or "Not a Bot"
+        username = "@" + from_user.username or "username not set"
+        status = from_user.status or "Status Not available"
+        dc_val  = from_user.dc_id or "Profile pic not set 🤒🤒"
+        if from_user.is_bot :
+         is_verify = from_user.is_verified or "Not a verified Bot"
+         restrictions = from_user.restrictions or "No Restrictions For this Bot"
+         message_out_str = ""
+         message_out_str += f"<b>ID:</b> <a href='tg://user?id={from_user.id}'>"
+         message_out_str += f"{from_user.id}"
+         message_out_str += "</a>\n"
+         message_out_str += f"<b>DC ID:</b> <code>{dc_val}</code>\n"
+         message_out_str += f"<b>Is Bot:</b> <code>{is_bot}</code>\n"
+         message_out_str += f"<b>Is Verified:</b> <code>{is_verify}</code>\n"
+         message_out_str += "<b>First Name:</b> "
+         message_out_str += f"<code>{first_name}</code>\n"
+         message_out_str += f"<b>Last Name:</b> <code>{last_name}</code>\n"
+         message_out_str += f"<b>Username:</b> {username}\n"
+         message_out_str += f"<b>Restrictions:</b> <code>{restrictions}</code>\n"
+        else:
+         is_verify = from_user.is_verified or "Not a verified User"
+         restrictions = from_user.restrictions or "No Restrictions For this User"
+         message_out_str = ""
+         message_out_str += f"<b>ID:</b> <a href='tg://user?id={from_user.id}'>"
+         message_out_str += f"{from_user.id}"
+         message_out_str += "</a>\n"
+         message_out_str += f"<b>DC ID:</b> <code>{dc_val}</code>\n"
+         message_out_str += f"<b>Is Bot:</b> <code>{is_bot}</code>\n"
+         message_out_str += f"<b>Is Scam:</b> <code>{scam}</code>\n"
+         message_out_str += f"<b>Is Verified:</b> <code>{is_verify}</code>\n"
+         message_out_str += "<b>First Name:</b> "
+         message_out_str += f"<code>{first_name}</code>\n"
+         message_out_str += f"<b>Last Name:</b> <code>{last_name}</code>\n"
+         message_out_str += f"<b>Username:</b> {username}\n"
+         message_out_str += f"<b>Status:</b> <code>{status}</code>\n"
+         message_out_str += f"<b>Mobile:</b> <code>{phone}</code>\n"
+         message_out_str += f"<b>Restrictions:</b> <code>{restrictions}</code>\n"
+
+         if message.chat.type in (("supergroup", "channel")):
+          try:
+            chat_member_p = await message.chat.get_member(from_user.id)
+            joined_date = datetime.fromtimestamp(
+                chat_member_p.joined_date or time.time()
+            ).strftime("%Y.%m.%d %H:%M:%S")
+            message_out_str += (
+                "<b>Joined On</b>: <code>"
+                f"{joined_date}"
+                "</code>\n"
             )
-            await message.reply_photo(
+          except UserNotParticipant:
+                pass
+         chat_photo = from_user.photo
+         if chat_photo:
+          local_user_photo = await client.download_media(message=chat_photo.big_file_id)
+          await message.reply_photo(
                 photo=local_user_photo,
                 quote=True,
                 caption=message_out_str,
@@ -69,9 +101,9 @@ async def who_is(client, message):
                 # ttl_seconds=,
                 disable_notification=True
             )
-            os.remove(local_user_photo)
-        else:
-            await message.reply_text(
+          os.remove(local_user_photo)
+         else:
+          await message.reply_text(
                 text=message_out_str,
                 quote=True,
                 parse_mode="html",
