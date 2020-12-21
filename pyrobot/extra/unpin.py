2c29790965c34a0350e37fbb9a6aaa086176fa26
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-12-21 13:55:35 +0530

    Update unpin.py

--- pyrobot/extra/unpin.py
+++ pyrobot/extra/unpin.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters
 
 @Client.on_message(filters.linked_channel & filters.incoming)
-async def autopin(Client, Message):
-    await Client.unpin_chat_message(
+async def autopin(client, message):
+    await message.unpin_chat_message(
     Message.chat.id
     )
