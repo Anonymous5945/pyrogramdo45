5f60f6a39beafdaaeb9cbf9aebaf8cbae2b48021
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2020-12-24 19:00:22 +0530

    Delete service.py

--- pyrobot/extra/service.py
+++
@@ -1,6 +0,0 @@
-from pyrogram.types import Message
-from pyrogram import Client,filters
-
-@Client.on_message(filters.service & filters.incoming)
-async def service(client, Message):
-    await Message.delete()
