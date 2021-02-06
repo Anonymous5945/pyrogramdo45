37165e45d6379cd0afe377c4621b48c9e7401acd
Anonymous5945 <56733554+Anonymous5945@users.noreply.github.com>
2021-02-06 10:49:54 +0530

    Create banr.py

---
+++ pyrobot/plugins/banr.py
@@ -0,0 +1,19 @@
+import time
+from pyrogram import Client, filters
+
+from pyrobot.pyrobot import PyroBot
+
+from pyrobot import AUTH_CHANNEL
+
+
+@PyroBot.on_message(filters.chat(chats=AUTH_CHANNEL) & filters.left_chat_member)
+async def banr_fn(client,message):
+   for member in message.left_chat_member:
+        try:
+              await message.chat.kick_member(
+                  user_id=member.id
+              )
+        except Exception as error:
+            await message.reply_text(
+                str(error)
+            )
